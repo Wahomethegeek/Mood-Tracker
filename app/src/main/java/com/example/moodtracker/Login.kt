16eb556a7e3e120146cc77e708652de408a54578
Wahomethegeek <gatwirikelvin@outlook.com>
2023-03-05 17:38:49 +0300

    initial commit

---
+++ app/src/main/java/com/example/moodtracker/Login.kt
@@ -0,0 +1,82 @@
+package com.example.moodtracker
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+
+fun LoginScreen(
+    onLoginClick: (String, String) -> Unit,
+    onSignUpClick: () -> Unit
+) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Welcome to My Mood Tracker",
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier.padding(16.dp)
+        )
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text(text = "Email") },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email
+            ),
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text(text = "Password") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password
+            ),
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        )
+
+        Button(
+            onClick = { onLoginClick(email, password) },
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) {
+            Text(text = "Log In")
+        }
+
+        TextButton(
+            onClick = onSignUpClick,
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(text = "Don't have an account? Sign up here")
+        }
+    }
+
+}
+
+
+
+
+
---
+++ app/src/main/java/com/example/moodtracker/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.moodtracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.moodtracker.ui.theme.MoodTrackerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MoodTrackerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = colorResource(id = R.color.background_color)
+                ) {
+                    //Log in page
+                    LoginScreen(onLoginClick =  {
+                        email, password ->
+                    },
+                        onSignUpClick = {
+
+
+                })
+
+            }
+        }
+    }
+}}
